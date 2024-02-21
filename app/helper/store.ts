a9e8c5a26d4ba99e6684634e6c6e39668f27cd68
JetX26 <kocevstanoj@gmail.com>
2024-02-21 00:53:45 -0600

    updates

--- app/helper/store.ts
+++ app/helper/store.ts
@@ -1,70 +1,67 @@
 import { create } from "zustand";
 import { StaticImageData } from "next/image";
-import VintageBg from '../assets/vintage-bg.png'
-import WeduBg from '../assets/wedu-bg.png'
-import JavascriptIcon from '../assets/js-icon.png'
-import ReactIcon from '../assets/react-icon.png'
-import VueIcon from '../assets/vue-icon.png'
-import TypeScriptIcon from '../assets/typescript-icon.png'
-import daisyUIIcon from '../assets/daisyui-logo-icon.png'
-import NextIcon from '../assets/next-js-icon.png'
-import TailwindIcon from '../assets/tailwind-css-icon.jpg'
-import firebaseIcon from '../assets/firebase-icon.png'
-import gitIcon from '../assets/git-icon.png'
-import githubIcon from '../assets/github-icon.png'
-import responsiveIcon from '../assets/responsive-icon.png'
-import ZustandIcon from '../assets/zustand-icon.jpg'
-import HTMLIcon from '../assets/html-icon.png'
-import CSSIcon from '../assets/css-icon.png'
-import WeDu from '../assets/wedu-card.png'
-import VintageCarGrg from '../assets/VintageGarageWebsiteCardImg.png'
-
+import VintageBg from "../assets/vintage-bg.png";
+import WeduBg from "../assets/wedu-bg.png";
+import JavascriptIcon from "../assets/js-icon.png";
+import ReactIcon from "../assets/react-icon.png";
+import VueIcon from "../assets/vue-icon.png";
+import TypeScriptIcon from "../assets/typescript-icon.png";
+import daisyUIIcon from "../assets/daisyui-logo-icon.png";
+import NextIcon from "../assets/next-js-icon.png";
+import TailwindIcon from "../assets/tailwind-css-icon.jpg";
+import firebaseIcon from "../assets/firebase-icon.png";
+import gitIcon from "../assets/git-icon.png";
+import githubIcon from "../assets/github-icon.png";
+import responsiveIcon from "../assets/responsive-icon.png";
+import ZustandIcon from "../assets/zustand-icon.jpg";
+import HTMLIcon from "../assets/html-icon.png";
+import CSSIcon from "../assets/css-icon.png";
+import WeDu from "../assets/wedu-card.png";
+import VintageCarGrg from "../assets/VintageGarageWebsiteCardImg.png";
+import MacasCookieHouse from "../assets/MacasCookieHse.png";
 
 interface zustandStoreInterface {
   aboutMeToggle: boolean;
   setAboutMeToggle: (data: boolean) => void;
-  myProjects: any,
-  languages: languagesInterface[],
-  frontEndFrameworks: frontEndFrameworksInterface[],
-  dataBases: databaseInterface[],
-  otherTools: otherToolsInterface[],
-  formInputs: string[],
-  sectionIds: string[],
-  myWork: myWorkInterface[]
+  myProjects: any;
+  languages: languagesInterface[];
+  frontEndFrameworks: frontEndFrameworksInterface[];
+  dataBases: databaseInterface[];
+  otherTools: otherToolsInterface[];
+  formInputs: string[];
+  sectionIds: string[];
+  myWork: myWorkInterface[];
 }
 
 interface languagesInterface {
-  title: string,
-  name: string,
-  icon: StaticImageData
+  title: string;
+  name: string;
+  icon: StaticImageData;
 }
 
 interface frontEndFrameworksInterface {
-  name: string,
-  icon: StaticImageData
+  name: string;
+  icon: StaticImageData;
 }
 
 interface databaseInterface {
-  name: string,
-  icon: StaticImageData
+  name: string;
+  icon: StaticImageData;
 }
 
 interface otherToolsInterface {
-  name: string,
-  icon: StaticImageData
+  name: string;
+  icon: StaticImageData;
 }
 
 interface myWorkInterface {
-  name: string,
-  link: string,
-  image: StaticImageData,
-  source: string
+  name: string;
+  link: string;
+  image: StaticImageData;
+  source: string;
 }
 
-
-
-
-export const useZustandStore = create < zustandStoreInterface > ((set, get) => ({
+export const useZustandStore = create<zustandStoreInterface>((set, get) => ({
   aboutMeToggle: false,
   setAboutMeToggle: (data) => {
     set({
@@ -73,93 +70,105 @@ export const useZustandStore = create < zustandStoreInterface > ((set, get) => (
   },
   myProjects: [
     {
-      name: 'Vintage Car Garage',
-      link: 'https://vintage-car-garage-template.netlify.app',
-      background: VintageBg
+      name: "Vintage Car Garage",
+      link: "https://vintage-car-garage-template.netlify.app",
+      background: VintageBg,
     },
     {
-      name: 'Wedu Template',
-      link: 'https://wedu-template.netlify.app',
-      background: WeduBg
-    }
+      name: "Wedu Template",
+      link: "https://wedu-template.netlify.app",
+      background: WeduBg,
+    },
   ],
   languages: [
     {
-      title: 'Languages',
-      name: 'Javascript',
-      icon: JavascriptIcon
+      title: "Languages",
+      name: "Javascript",
+      icon: JavascriptIcon,
     },
     {
-      title: '',
-      name: 'HTML5',
-      icon: HTMLIcon
+      title: "",
+      name: "HTML5",
+      icon: HTMLIcon,
     },
     {
-      title: '',
-      name: 'CSS',
-      icon: CSSIcon
-    }
+      title: "",
+      name: "CSS",
+      icon: CSSIcon,
+    },
   ],
   frontEndFrameworks: [
     {
-      name: 'React.js',
-      icon: ReactIcon
+      name: "React.js",
+      icon: ReactIcon,
     },
     {
-      name: 'Next.js',
+      name: "Next.js",
       icon: NextIcon,
     },
     {
-      name: 'Vue.js',
-      icon: VueIcon
+      name: "Vue.js",
+      icon: VueIcon,
     },
     {
-      name: 'Typescript',
-      icon: TypeScriptIcon
+      name: "Typescript",
+      icon: TypeScriptIcon,
     },
     {
-      name: 'Tailwind CSS',
-      icon: TailwindIcon
+      name: "Tailwind CSS",
+      icon: TailwindIcon,
     },
     {
-      name: 'Zustand',
-      icon: ZustandIcon
-    }
+      name: "Zustand",
+      icon: ZustandIcon,
+    },
   ],
   dataBases: [
     {
-      name: 'Firebase',
-      icon: firebaseIcon
-    }
+      name: "Firebase",
+      icon: firebaseIcon,
+    },
   ],
   otherTools: [
     {
-      name: 'Git',
-      icon: gitIcon
+      name: "Git",
+      icon: gitIcon,
     },
     {
-      name: 'Github',
-      icon: githubIcon
+      name: "Github",
+      icon: githubIcon,
     },
     {
-      name: 'Responsive Web Design',
-      icon: responsiveIcon
-    }
+      name: "Responsive Web Design",
+      icon: responsiveIcon,
+    },
   ],
-  formInputs: ['First_Name', 'Last_Name', 'Company', 'Email_Address', 'Phone_Number'],
-  sectionIds: ['Contact', 'Stack'],
+  formInputs: [
+    "First_Name",
+    "Last_Name",
+    "Company",
+    "Email_Address",
+    "Phone_Number",
+  ],
+  sectionIds: ["Contact", "Stack"],
   myWork: [
     {
-      name: 'Wedu Template',
-      link: 'https://wedu-template.netlify.app/',
+      name: "Wedu Template",
+      link: "https://wedu-template.netlify.app/",
       image: WeDu,
-      source: 'https://github.com/JetX26/Wedu-Template'
+      source: "https://github.com/JetX26/Wedu-Template",
     },
     {
-      name: 'Vintage Car Garage',
-      link: 'https://vintage-car-garage-template.netlify.app/',
+      name: "Vintage Car Garage",
+      link: "https://vintage-car-garage-template.netlify.app/",
       image: VintageCarGrg,
-      source: 'https://github.com/JetX26/Vintage-Car-Garage'
-    }
-  ]
+      source: "https://github.com/JetX26/Vintage-Car-Garage",
+    },
+    {
+      name: "Macas Cookie House",
+      link: "https://macascookiehouse.netlify.app/",
+      image: MacasCookieHouse,
+      source: "https://github.com/JetX26/Macas-Cookie-House",
+    },
+  ],
 }));
