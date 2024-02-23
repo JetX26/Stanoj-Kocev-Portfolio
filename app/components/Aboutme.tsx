c43f33ec16d34acd8e1e365c2643c570b3a155a6
JetX26 <kocevstanoj@gmail.com>
2024-02-23 00:19:01 -0600

    fixes

--- app/components/Aboutme.tsx
+++ app/components/Aboutme.tsx
@@ -1,14 +1,14 @@
 'use client'
 
 import React from 'react'
-import StanojKocevPhoto from '../assets/linkedin photo.jpg'
+import myPhoto from '../assets/skocev.png'
 import Image from 'next/image'
 
 const Aboutme = (): React.ReactNode => {
 
     return (
         <div className="card card-side bg-base-100 shadow-xl">
-            <figure className='w-1/2'><Image src={StanojKocevPhoto} alt="Movie"></Image></figure>
+            <figure className='w-1/2'><Image src={myPhoto} alt="Movie"></Image></figure>
             <div className="card-body w-1/2">
                 <p>{`Hey there! 👋 I’m Stanoj, and I’m all about building cool stuff on the internet. I get a kick out of creating websites that look awesome and work smoothly. It all started with me being curious about how websites work, and now I’m hooked! I spend my time coding and making things look neat online. When I’m not glued to the screen, you might catch me checking out the latest tech trends or just chilling outdoors. Let’s team up and make your online dreams come true!`}</p>
                 <div className="card-actions justify-end">
